0f426ff68fc62d8958662e8607ddf1f2958c0652
xBarikadex <71157911+xBarikadex@users.noreply.github.com>
2021-10-05 16:13:44 -0700

    Started

---
+++ tours-assignment/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function App() {
+    return (
+        <div>
+            <p>abcdefghijklmnopqrstuvwxyz</p>
+            <p>1234567890</p>
+            <p>!@#$%^&*()-_=+</p>
+            <p>[]{}\|;:'"</p>
+            <p>********</p>
+        </div>
+    )
+}
+
+export default App
