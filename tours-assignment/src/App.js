70afefb926fed330dce82c158a0f041db364a4d7
xBarikadex <71157911+xBarikadex@users.noreply.github.com>
2021-10-12 17:25:37 -0700

    Got Tour Cards to Work

--- tours-assignment/src/App.js
+++ tours-assignment/src/App.js
@@ -1,24 +1,41 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import TourBox from './Components/TourBox';
 import ErrorPage from './Components/ErrorPage';
 import LoadingPage from './Components/LoadingPage';
 
 let url = "https://course-api.com/react-tours-project";
-let name;
+// let name;
 
 function App() {
     const [isLoading, setIsLoading] = useState(false);
     const [isError, setIsError] = useState(false);
+    const [city, setCity] = useState([]);
 
-    fetch(url)
-    .then(response => {
-        console.log('Tour');
-        name = response.json();
-        console.log(name);  
-    })
-    .catch(err => {
-        console.log(err);
-    })
+    const getCity = async () => {
+        const response = await fetch(url);
+        const city = await response.json();
+        // console.log(city);
+        setCity(city);
+    }
+
+    useEffect(() => {
+        getCity()
+            .then(setIsLoading(false))
+            .catch((error) => {
+                console.log(error);
+                setIsError(true);
+            })
+    },[])
+    
+    // fetch(url)
+    // .then(response => {
+    //     console.log('Tour');
+    //     name = response.json();
+    //     console.log(name);
+    // })
+    // .catch(err => {
+    //     console.log(err);
+    // })
 
     if (isLoading === true) {
         return <LoadingPage />
@@ -26,13 +43,15 @@ function App() {
     if (isError === true) {
         return <ErrorPage />
     }
-    console.log('name2');
-    console.log(name);
 
+    // console.log(city);
     return (
         <>
-            <div>
-                <TourBox data={name} /* key={destination.id} *//>
+            <h1 id='title'>Our Tours</h1>
+            <div className='cityCards'>
+                {city.map((card) => {
+                    return <TourBox data={card} key={card.id}/>
+                })}
             </div>
         </>
     )
--- tours-assignment/src/Components/TourBox.js
+++ tours-assignment/src/Components/TourBox.js
@@ -1,17 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function TourBox({data}) {
-    // const { id, image, info, name, price } = data;
-    console.log('Destination');
-    console.log(data);
+    const { image, info, name, price } = data;
+    // console.log('Destination');
+    // console.log(data);
+
+    function expandText(text) {
+        let letterCount = text.info.split('').length;
+        console.log(letterCount);
+        if (letterCount >= 200) {
+            info = 'More than 200 letters'
+        } else {
+            info = 'Less than 200 letters'
+        }
+    }
+
+    function removeCard() {
+
+    }
+
+    const [readMore, setReadMore] = useState(false);
+    const extraContent = <div>
+        <p className="extra-content">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, consectetur neque ab 
+            porro quasi culpa nulla rerum quis minus voluptatibus sed hic ad quo sint, libero 
+            commodi officia aliquam! Maxime.
+        </p>
+    </div>
+    const linkName = readMore?'Read Less << ':'Read More >> '
 
     return (
-        <div>
-            {/* <p>{id}</p>
-            <p>{image}</p>
-            <p>{info}</p>
-            <p>{name}</p>
-            <p>{price}</p> */}
+        <div className='cardContainer'>
+            <button onClick={() => expandText({info})}>Hallo</button>
+            <img className='image' src={image} alt={name} />
+            <p className='name'>{name}</p>
+            <p className='price'>{price}</p>
+            <p className='info'>
+                {info}
+                <a className="read-more-link" onClick={()=>{setReadMore(!readMore)}}><h2>{linkName}</h2></a>
+                {readMore && extraContent}
+            </p>
+            <button onClick={() => removeCard()}>Not Interested</button>
         </div>
     )
 }
